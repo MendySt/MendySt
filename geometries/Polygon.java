506abf3c0c78edb87c4f4cf7c11334d446dc1c00
MendySt <mendi7272@gmail.com>
2020-04-19 11:15:55 +0300

    17 / 04

--- geometries/Polygon.java
+++ geometries/Polygon.java
@@ -81,7 +81,7 @@ public class Polygon implements Geometry {
     }
 
     @Override
-    public Vector getNormal(Point3D point3D) {
+    public Vector getNormal(Point3D point) {
         return _plane.getNormal();
     }
 }
